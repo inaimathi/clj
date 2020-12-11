06682ecf822d23455fc9c2cffc588be1f6aa3285
inaimathi <leo.zovic@gmail.com>
2020-12-11 12:41:47 -0500

    Add string template syntax

--- src/syntax.lisp
+++ src/syntax.lisp
@@ -39,11 +39,32 @@
   (declare (ignore sub-char numarg))
   `(list->set (list ,@(read-delimited-list #\} stream t))))
 
+;;;;;;;;;; String Templates
+(defun string-template-reader (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (let ((res (read stream)))
+    (assert (stringp res) nil "You need to provide a string to the #F reader")
+    (if (contains? res #\{)
+	(let ((chunks nil)
+	      (chunk nil))
+	  (with-input-from-string (s res)
+	    (loop for c = (read-char s nil nil) while c
+		  do (if (char= #\{ c)
+			 (progn
+			   (push (coerce (reverse chunk) 'string) chunks)
+			   (setf chunk nil)
+			   (push (eval (read s)) chunks)
+			   (read-char s))
+			 (push c chunk))))
+	  (format nil "~{~a~}" (reverse chunks)))
+	res)))
+
 ;;;;;;;;;; Readtable definition
 (named-readtables:defreadtable syntax
   (:merge :standard)
   (:macro-char #\{ #'map-literal-reader nil)
   (:macro-char #\} (get-macro-character #\)) nil)
+  (:dispatch-macro-char #\# #\f #'string-template-reader)
   (:dispatch-macro-char #\# #\{ #'set-literal-reader))
 
 (local-package-aliases:set-aliasing-reader (named-readtables:find-readtable 'clj:syntax))
